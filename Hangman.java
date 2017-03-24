0c26a5cea90d1fb05c6a46a8cd308afe12887624
Dan PElensky <dan@pelensky.com>
2017-03-24 11:13:03 +0000

    the command line shows the number of letters in the word

--- Hangman.java
+++ Hangman.java
@@ -1,7 +1,10 @@
 class Hangman {
  
   public static void main(String[] args) {
-    String word = "Timmy";
-    System.out.println(word);
+    String word = "TIMMY";
+    String line = "-------------------------";
+    System.out.println(line);
+    System.out.println("Welcome to Hangman!");
+    System.out.println("The word is " + word.length() + " characters long");
   }
 }
