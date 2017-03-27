24310bad3116227eaeb7bfa635ed211de4a7209a
Dan Pelensky <dan@pelensky.com>
2017-03-27 10:44:52 +0100

    the underlines are displayed for the length of the word

--- Hangman.java
+++ Hangman.java
@@ -6,5 +6,9 @@ class Hangman {
     System.out.println(line);
     System.out.println("Welcome to Hangman!");
     System.out.println("The word is " + word.length() + " characters long");
+    String wordLength;
+    for(int i=0; i<word.length(); i++){
+    System.out.print(" _");
+}
   }
 }
