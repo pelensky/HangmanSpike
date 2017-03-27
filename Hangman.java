aba10591978eaa1a27da52dc6830f60b7ead501c
Dan Pelensky <dan@pelensky.com>
2017-03-27 12:01:54 +0100

    basic MVP

--- Hangman.java
+++ Hangman.java
@@ -1,13 +1,48 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 class Hangman {
- 
+
   static public void guess(String word, int lives){
     char[] emptySpaces = new char[word.length()];
-    
-   for(int i=0; i < word.length(); i++){
+
+    for(int i=0; i < word.length(); i++){
       emptySpaces[i] = '_';
     }
+
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Character> listOfCharacters = new ArrayList<Character>();
+
+    while(lives>0){
+      char x = scanner.next().charAt(0);
+      if(listOfCharacters.contains(x)){
+        System.out.println("You have already guessed " + x);
+        continue;
+      }
+      listOfCharacters.add(x);
+
+      if(word.contains(x + "")){
+        for (int y = 0; y < word.length(); y++){
+          if(word.charAt(y)==x){
+            emptySpaces[y]=x;
+          }
+        }
+      }
+      else {
+        lives -= 1;
+      }
+    if(word.equals(String.valueOf(emptySpaces))){
+      System.out.println(emptySpaces);
+      System.out.println("You are the champion!");
+      break;
+    }
+
     System.out.print(emptySpaces);
     System.out.println("Lives remaining: " + lives);
+    }
+    if(lives==0){
+      System.out.println("You Lose");
+    }
   }
 
   public static void main(String[] args) {
